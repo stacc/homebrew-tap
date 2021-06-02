30d5639712ebe55f8dcc786db872ed65a0b91fb2
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-02 09:34:56 +0200

    Brew formula update for stacc version v0.1.86

--- Formula/stacc.rb
+++ Formula/stacc.rb
@@ -5,25 +5,25 @@
 class Stacc < Formula
   desc "Software used to communicate with Stacc cloud environments"
   homepage "https://github.com/stacc/stacc-CLI"
-  version "0.1.85"
+  version "0.1.86"
   license "Apache-2.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/stacc/stacc-CLI/releases/download/v0.1.85/stacc_0.1.85_Darwin_x86_64.tar.gz"
-    sha256 "6daf822cdf8a6c6df3f1f5154c20598c2b46aae3c57c8bf89d0f1fc01f5e836a"
+    url "https://github.com/stacc/stacc-CLI/releases/download/v0.1.86/stacc_0.1.86_Darwin_x86_64.tar.gz"
+    sha256 "26a53108c780c3b7c926d516fbfc8de5b507e6f657d39b523a21cad1a17208e5"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/stacc/stacc-CLI/releases/download/v0.1.85/stacc_0.1.85_Darwin_arm64.tar.gz"
-    sha256 "41826b614ed9ea9e3e2fa5c5165b1a8056400925f4a1627d93d74cfacbcc0e7e"
+    url "https://github.com/stacc/stacc-CLI/releases/download/v0.1.86/stacc_0.1.86_Darwin_arm64.tar.gz"
+    sha256 "836f414a47b8da966e84cd4dcf3163760c0f8639ab6fa557299726ecd446edf9"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/stacc/stacc-CLI/releases/download/v0.1.85/stacc_0.1.85_Linux_x86_64.tar.gz"
-    sha256 "3a6336d4ddd400c6233cc7fc381561a1f29671df9c6a0e4f7fae283b73fac850"
+    url "https://github.com/stacc/stacc-CLI/releases/download/v0.1.86/stacc_0.1.86_Linux_x86_64.tar.gz"
+    sha256 "b08e0da9ff60963161958accc0d29f72f7e16e40955ca93f0e79ad5e9ee6e59c"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/stacc/stacc-CLI/releases/download/v0.1.85/stacc_0.1.85_Linux_arm64.tar.gz"
-    sha256 "f10e58933f6ba714e6c41311d45277600fa836028aec0540a36ab24f62cd877d"
+    url "https://github.com/stacc/stacc-CLI/releases/download/v0.1.86/stacc_0.1.86_Linux_arm64.tar.gz"
+    sha256 "664c3d47590d250cbaaaaff149596584f41a6b018992c52029e521638a4bae73"
   end
 
   def install
