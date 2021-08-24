75e8ae43e51393fbf354679783042595bffe3695
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-24 10:43:15 +0200

    Brew formula update for stacc version v0.2.41

--- Formula/stacc.rb
+++ Formula/stacc.rb
@@ -5,29 +5,29 @@
 class Stacc < Formula
   desc "Software used to communicate with Stacc cloud environments"
   homepage "https://github.com/stacc/stacc-CLI"
-  version "0.2.40"
+  version "0.2.41"
   license "Apache-2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/stacc/cli/releases/download/v0.2.40/stacc_0.2.40_Darwin_x86_64.tar.gz"
-      sha256 "bfb12a5a6b986494aeff4eaf9c927469cd329beb7c029d9a57e858d67660f16e"
+      url "https://github.com/stacc/cli/releases/download/v0.2.41/stacc_0.2.41_Darwin_x86_64.tar.gz"
+      sha256 "b713facc92f0d8c68cd5330df4e1409a1d38f6d4be3f7cc495888d2c36305370"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/stacc/cli/releases/download/v0.2.40/stacc_0.2.40_Darwin_arm64.tar.gz"
-      sha256 "5afde53517439288133f4015dda105b3edde9ca801ea24147fcf72fa0f480240"
+      url "https://github.com/stacc/cli/releases/download/v0.2.41/stacc_0.2.41_Darwin_arm64.tar.gz"
+      sha256 "63c797df95d390464cb5f45583d96e4cdf625ae84b1e51894898f4bf8573d7ef"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/stacc/cli/releases/download/v0.2.40/stacc_0.2.40_Linux_x86_64.tar.gz"
-      sha256 "1ae151dbff151b2345b6b58e1815574e79696bb55e8247ea9bfceb1af4f0334a"
+      url "https://github.com/stacc/cli/releases/download/v0.2.41/stacc_0.2.41_Linux_x86_64.tar.gz"
+      sha256 "9c5c55204b07a12d492d51f8a6abcfa589ac36144d529bafff3c9e79c944ee24"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/stacc/cli/releases/download/v0.2.40/stacc_0.2.40_Linux_arm64.tar.gz"
-      sha256 "a281e6d603f7f75966e3d0584ba0190f9828250337e5aa82318b02b6bfc2e428"
+      url "https://github.com/stacc/cli/releases/download/v0.2.41/stacc_0.2.41_Linux_arm64.tar.gz"
+      sha256 "7eab97eeb9618da8da56bcc4da2692c678282ad7a6741e7d51711ca261b28126"
     end
   end
 
