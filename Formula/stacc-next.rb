f17940d0388659bd99e9ab47922d22bf0e4cf7d5
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-08 12:32:41 +0200

    Brew formula update for stacc version v0.4.25

--- Formula/stacc-next.rb
+++ Formula/stacc-next.rb
@@ -5,29 +5,29 @@
 class StaccNext < Formula
   desc "Software used to communicate with Stacc cloud environments"
   homepage "https://github.com/stacc/cli"
-  version "0.4.22"
+  version "0.4.25"
   license "Apache-2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/stacc/cli-next/releases/download/v0.4.22/stacc_0.4.22_Darwin_arm64.tar.gz"
-      sha256 "846b9dbe06cf2abdf2302d808badf51a52edb5765f839b68fa2fa2bd2b85a752"
+      url "https://github.com/stacc/cli-next/releases/download/v0.4.25/stacc_0.4.25_Darwin_arm64.tar.gz"
+      sha256 "f7b156374ab6ccd86e4dea27544a3f84735e383468480bbae27ad17c09cb3ec7"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/stacc/cli-next/releases/download/v0.4.22/stacc_0.4.22_Darwin_x86_64.tar.gz"
-      sha256 "8192fbe51366a40e24b735194665138d04ea347c874b077bdd49c0c6c82b0d4c"
+      url "https://github.com/stacc/cli-next/releases/download/v0.4.25/stacc_0.4.25_Darwin_x86_64.tar.gz"
+      sha256 "c956a3ff8df61d7b6ab6cfc780fbd1fc4fd759ff1e00746d3129a06b0a5c24c7"
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/stacc/cli-next/releases/download/v0.4.22/stacc_0.4.22_Linux_arm64.tar.gz"
-      sha256 "98bc805d024b4deeae5b3641a97d115327ae62e4b230da1a6ab06997d002f36c"
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/stacc/cli-next/releases/download/v0.4.22/stacc_0.4.22_Linux_x86_64.tar.gz"
-      sha256 "bd928596de715c54bd0d99a54f6d5162ecdc19870c6eb1b16d89f63463aa1a9e"
+      url "https://github.com/stacc/cli-next/releases/download/v0.4.25/stacc_0.4.25_Linux_x86_64.tar.gz"
+      sha256 "0638a395a40b74dce23c5ff76516fbefcec950d6c1ede3f76365837e5ac7c150"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/stacc/cli-next/releases/download/v0.4.25/stacc_0.4.25_Linux_arm64.tar.gz"
+      sha256 "78d9f55a4d9383c50832c9883f16362bb444c845ebd218276bab749df4bc946a"
     end
   end
 
