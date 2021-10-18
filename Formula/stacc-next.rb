f5f7f575d8a621e595491f7d4ab07beee9a9596e
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-18 11:57:55 +0200

    Brew formula update for stacc version v0.4.29

--- Formula/stacc-next.rb
+++ Formula/stacc-next.rb
@@ -5,29 +5,29 @@
 class StaccNext < Formula
   desc "Software used to communicate with Stacc cloud environments"
   homepage "https://github.com/stacc/cli"
-  version "0.4.28"
+  version "0.4.29"
   license "Apache-2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/stacc/cli-next/releases/download/v0.4.28/stacc_0.4.28_Darwin_x86_64.tar.gz"
-      sha256 "04db9950cf77b2c8348b9aa3938e55c196a973503a395393bdce0109c5c67134"
+      url "https://github.com/stacc/cli-next/releases/download/v0.4.29/stacc_0.4.29_Darwin_x86_64.tar.gz"
+      sha256 "d79569066b8d60bf3cd32d62c0ace430112d324965d2de0bbfeec570082139a1"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/stacc/cli-next/releases/download/v0.4.28/stacc_0.4.28_Darwin_arm64.tar.gz"
-      sha256 "77151903f149b9de8c8e4d9b37cd8a3e8fdc786538d3d049fe221dc5ee25a5cd"
+      url "https://github.com/stacc/cli-next/releases/download/v0.4.29/stacc_0.4.29_Darwin_arm64.tar.gz"
+      sha256 "a582bacec99d3416b4dcdb4430a3ee21f53ab7674e700c77b7a1c983c6de5b71"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/stacc/cli-next/releases/download/v0.4.28/stacc_0.4.28_Linux_x86_64.tar.gz"
-      sha256 "c774bf859385cf136b36e4ad3cedc66a6e116101d0a863b848b371ea03cb6278"
+      url "https://github.com/stacc/cli-next/releases/download/v0.4.29/stacc_0.4.29_Linux_x86_64.tar.gz"
+      sha256 "62ed300a6fbd2646eb057f424538374a6d9e517d9998a7ad7c282b931fcb1d71"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/stacc/cli-next/releases/download/v0.4.28/stacc_0.4.28_Linux_arm64.tar.gz"
-      sha256 "e36a09d9106fe1508c725d66efc4da80e9012ac64a2f33403e54c4ad98d8eed1"
+      url "https://github.com/stacc/cli-next/releases/download/v0.4.29/stacc_0.4.29_Linux_arm64.tar.gz"
+      sha256 "a9595aac127c992451220e9be5768fe083e32f065a5f6d4b96da6a54689123f2"
     end
   end
 
